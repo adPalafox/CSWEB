d69f89159d85373dfec76e2cebc2368a465b5f52
Curfy <kennethrada93@gmail.com>
2022-02-25 16:57:44 +0800

    test

---
+++ login/index.php
@@ -0,0 +1,33 @@
+<?php
+
+session_start();
+
+// if(isset($_COOKIE["type"]))
+// 	if($_COOKIE["type"]=="patient")
+// 		header("location:logout.php");
+	
+// if(isset($_COOKIE["id"])){
+// 	header("location:dashboard.php");
+// }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="maximum-scale=1.0, width=device-width, initial-scale=1.0">
+	<title>Login Module</title>
+
+	<?php include 'assets/links.php'; ?>
+
+</head>
+
+<body>
+	<div class="container">
+		<?php include 'login.php'; ?>
+	</div>
+</body>
+
+</html>

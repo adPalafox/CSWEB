baabd25770e94daefd9b1575c1abfc9e2fe0c7dc
AApalafox <adreanpalafox@gmail.com>
2022-02-26 22:24:26 +0800

    added the fcking jQuery link

--- recipeindex/profilepage.php
+++ recipeindex/profilepage.php
@@ -8,6 +8,7 @@
 	<title>Account</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+	<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"> </script>
 	<link rel="stylesheet" href="profilepagestyle.css">
 </head>
 
@@ -500,7 +501,7 @@
 	if (username != "") {
 		$("#loginForm").append('<a href="./profilepage.php"> ' + username + '</a>');
 		$("#loginForm").append('<a href="./index.php"><button id = "logoutBtn" class="logout">Log Out</button></a>');
-		$("#navigation").append('<li><a href="./form/addrecipe.php">Create Recipe</a></li>');
+		$("#navigation").append('<li><a href="form/addrecipe.php">Create Recipe</a></li>');
 	} else {
 		$("#loginForm").append('<a href = "./login/login.php">Log In</a>');
 		$("#loginForm").append('<a href = "./login/signup.php"><button class="signup">Sign Up →</button></a>');

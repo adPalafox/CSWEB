9dd11a0ba98d1df3e7502c41af60d068e92fbe06
Curfy <kennethrada93@gmail.com>
2022-02-28 11:37:48 +0800

    Update index.php

--- recipeindex/home/index.php
+++ recipeindex/home/index.php
@@ -136,7 +136,7 @@
     if (username != "") {
         $("#loginForm").append('<a href="./profilepage.php"> ' + username + '</a>');
         $("#loginForm").append('<a href="./index.php"><button id = "logoutBtn" class="logout">Log Out</button></a>');
-        $("#navigation").append('<li><a href="./form/addrecipe.php">Create Recipe</a></li>');
+        $("#navigation").append('<li><a href="../form/addrecipe.php">Create Recipe</a></li>');
         $("#createRecipe").append('<a href="#">Create a Recipe</a>');
     } else {
         $("#loginForm").append('<a href = "../login/index.php">Log In</a>');

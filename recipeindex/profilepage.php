7f8a0a0f1267adb21b09015f52e04851586b52da
AApalafox <adreanpalafox@gmail.com>
2022-03-02 19:06:19 +0800

    updated design of profile page

--- recipeindex/profilepage.php
+++ recipeindex/profilepage.php
@@ -5,7 +5,7 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Account</title>
+	<title>Tasty Profile</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 	<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"> </script>
@@ -22,7 +22,7 @@
 	$maxattempts = 3;
 	$expireResult = "";
 	$message = '';
-	
+
 	if (!isset($_COOKIE['page'])) {
 		setcookie("page", "login", time() + 3600);
 	}
@@ -44,11 +44,10 @@
 	// ERROR MESSAGE
 	if (isset($_COOKIE['attempt'])) {
 		if ($_COOKIE['attempt'] < $maxattempts) {
-			if($_COOKIE['attempt'] > 0){
+			if ($_COOKIE['attempt'] > 0) {
 				$message = '<div class="alert alert-danger"> Incorrect Login: (' . $_COOKIE["attempt"] . "/" . $maxattempts . ')</div>';
 			}
-		}
-		else{
+		} else {
 			$message = '<div class="alert alert-danger"><span id = "time"> Login Attempt Limit Reached!</span></div>';
 		}
 	}
@@ -153,29 +152,30 @@
 		}
 	}
 
-	function validateDate($datenow, $sqldate){
+	function validateDate($datenow, $sqldate)
+	{
 		$days = 0;
 		$datenow = strtotime($datenow);
 		$sqldate = strtotime($sqldate);
 
 		$datediff = $sqldate - $datenow;
 
-    	$days = abs(round($datediff / (60 * 60 * 24)));
+		$days = abs(round($datediff / (60 * 60 * 24)));
 
 		return $days;
 	}
 
-	function getDays($now, $sqldate){
+	function getDays($now, $sqldate)
+	{
 
 		$datediff = $sqldate - $now;
-		
+
 		$days = abs(round($datediff / (60 * 60 * 24)));
-		
-		echo $days." days";
-		
+
+		echo $days . " days";
 	}
 
-	$message = '<strong>Welcome '.base64_decode($_COOKIE['user']).'</strong>';
+	$message = '<strong>Welcome ' . base64_decode($_COOKIE['user']) . '</strong>';
 
 	// RESET PASSWORD
 	if (isset($_POST["resetPassword"])) {
@@ -216,34 +216,6 @@
 		}
 	}
 	?>
-	<!-- Nav -->
-	<!-- <nav class="navbar navbar-expand-lg navbar-light bg-light">
-		<div class="container">
-			<img src="./home/All-Tasty-.png" alt="logo" width="50" height="40">
-			<a class="navbar-brand" href="#">All Tasty</a>
-			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
-			<div class="collapse navbar-collapse" id="navbarSupportedContent">
-				<ul class="navbar-nav me-auto p-3 mb-2 mb-lg-0">
-					<li class="nav-item ms-5">
-						<a class="nav-link" href="#">Home</a>
-					</li>
-					<li class="nav-item ms-5">
-						<a class="nav-link" href="#">Recipes</a>
-					</li>
-					<li class="nav-item ms-5">
-						<a class="nav-link" href="#">About</a>
-					</li>
-					<li class="nav-item ms-5">
-						<a class="nav-link" href="#">Create Recipe</a>
-					</li>
-
-				</ul>
-				<button class="btn btn-dark" type="submit">Logout</button>
-			</div>
-		</div>
-	</nav> -->
 	<div class="mycontainer">
 		<div class="mynavbar">
 			<div class="logo">
@@ -261,30 +233,23 @@
 			</div>
 		</div>
 	</div>
-	
+
 	<!-- End of Nav -->
 
-	<div class="bg-profile-card">
+	<!--Profile card-->
+	<div class="text-center">
 		<div class="container">
-			<!--Profile card-->
-			<div class="">
-				<div class="row g-0">
-					<div class="col-md-3 mt-2">
+			<div class="my-card">
+				<div class="row g-0 justify-content-center">
+					<div class="col-lg align-self-center">
 						<img src="./about/merin.jpg" class="mx-auto d-block img-circle" alt="profile picture">
 					</div>
-					<div class="col-6">
-						<div class="card-body" id = profile>
-
-						</div>
-
+					<div class="col-lg-5">
+						<div class="" id=profile></div>
 					</div>
-
-					<div class="pics">
-						<div class="pics-group">
-							<img class="rightcenter" src="./home/mushcenter.png">
-							<img class="rightcenter2" src="./home/cilantro.png">
-							<img class="rightcenter3" src="./home/parsley.png">
-							<img class="right" src="./home/broccoliright.png">
+					<div class="col-lg align-self-center">
+						<div class="profile2">
+							<p>Keep your account secure by regulary changing your password</p>
 						</div>
 					</div>
 				</div>
@@ -293,9 +258,9 @@
 	</div>
 
 	<!--End of profile card-->
-	<div class="container">
 
-		<!--Posted Recipes-->
+	<!--Posted Recipes-->
+	<div class="container">
 		<h3 class="">Created Recipes</h3>
 		<div class="row row-cols-1 row-cols-md-4 g-4">
 			<div class="col">
@@ -390,7 +355,7 @@
 
 								<label for="forgotemail" class="form-label" style="color: red">Email Address</label>
 								<div class="input-group flex-nowrap mb-3">
-									<input type="email" class="form-control" placeholder="Email" id="forgotemail" name="forgotemail" value="<?php echo base64_decode($_COOKIE['email'])?>" readonly>
+									<input type="email" class="form-control" placeholder="Email" id="forgotemail" name="forgotemail" value="<?php echo base64_decode($_COOKIE['email']) ?>" readonly>
 									<label class="input-group-text" id="addon-wrapping" for="email"><i class="fas fa-user fa-1x p-2"></i></label>
 								</div>
 
@@ -454,34 +419,34 @@
 	$("#logoutBtn").click(function() {
 		if (username != "") {
 			document.cookie = `id= ;expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/`;
-            document.cookie = `user= ;expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/`;
-            document.cookie = `email= ;expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/`;
+			document.cookie = `user= ;expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/`;
+			document.cookie = `email= ;expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/`;
 			location.reload();
 		}
 	});
-	
-	function loadProfile(id){
-        $.ajax({
-        url: "./loadprofile.php",
-        type: "POST",
-        data:{
-            "id": (id),
-        },
-        success: function(response){
-			var firstname = atob(response[0].firstname);
-			var lastname = atob(response[0].lastname);
-			var email = atob(response[0].email);
-			// document.cookie = "email="+response[0].email;
-			
-			// $("#profile").append('<h1>'+recipe.recipe_name+'</h1> <p>'+recipe.recipe_description+'</p> <div class="time-grid">  <div id = "recipeServings" class="time-square-1"> <div class="time-title-1">Serving</div> <div class="time-alotted">'+recipe.servings+' servings</div></div><div id = "recipeTime" class="time-square-2"><div class="time-title-2">Cook</div><div class="time-alotted">'+cooktime+'</div></div></div>')
-            // <p class="profile-name"> </p>
-			$("#profile").append('<p class="profile-name">'+firstname+' '+lastname+'</p>');
-			$("#profile").append('<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In nihil beatae aspernatur ipsam quam debitis? </p>');
-			$("#profile").append('<p class="profile-info">'+email+'</p>');
-			$("#profile").append('<a class="mylink" data-bs-toggle="modal" data-bs-target="#modalResetPass"> Reset Password </a>');
+
+	function loadProfile(id) {
+		$.ajax({
+			url: "./loadprofile.php",
+			type: "POST",
+			data: {
+				"id": (id),
+			},
+			success: function(response) {
+				var firstname = atob(response[0].firstname);
+				var lastname = atob(response[0].lastname);
+				var email = atob(response[0].email);
+				// document.cookie = "email="+response[0].email;
+
+				// $("#profile").append('<h1>'+recipe.recipe_name+'</h1> <p>'+recipe.recipe_description+'</p> <div class="time-grid">  <div id = "recipeServings" class="time-square-1"> <div class="time-title-1">Serving</div> <div class="time-alotted">'+recipe.servings+' servings</div></div><div id = "recipeTime" class="time-square-2"><div class="time-title-2">Cook</div><div class="time-alotted">'+cooktime+'</div></div></div>')
+				// <p class="profile-name"> </p>
+				$("#profile").append('<p class="profile-name underline">' + firstname + ' ' + lastname + '</p>');
+				$("#profile").append('<p class="profile-info">Click to change me. I am person description that you can edit.</p>');
+				$(".profile2").append('<p class="underline">' + email + '</p>');
+				$(".profile2").append('<a class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#modalResetPass"> Reset Password </a>');
 			}
-        });
-    }
-	
+		});
+	}
+
 	loadProfile(getCookie('id'));
 </script>
